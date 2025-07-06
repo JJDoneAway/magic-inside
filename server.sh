36c236835cc5920f010d3953c3e18b536ed53a3a
Hoehne Johannes <johannes.hoehne@metrosystems.net>
2025-07-06 16:52:36 +0200

    Added rawpixel.com reference back to footer and updated consistent styling for Impressum and To the Top section

--- server.sh
+++ server.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
-
+lsof -i :8000 | awk 'NR>1 {print $2}' | xargs kill -9
 # Start a local web server and clear browser cache
 python3 -m http.server 8000 &
-SERVER_PID=$!
 
-# Watch for changes in the workspace and refresh the browser
-fswatch -o . | while read f; do
-    kill -HUP $SERVER_PID
-    python3 -m http.server 8000 &
-    SERVER_PID=$!
-    echo "Server refreshed due to changes."
-    curl -X PURGE http://localhost:8000
-done
